b6af6a47de68918efad1d639312f449cfcbfb5da
adrianmulaj <58852141+adrianmulaj@users.noreply.github.com>
2020-11-10 23:09:06 +0100

    risolti problemi di buffering...

--- esonero.c
+++ esonero.c
@@ -12,14 +12,13 @@ char key[129];
 char m_output[129];
 
 int main (){
+    int s;
 
-  printf("Inserisci una stringa NON piu lunga di 128 caratteri:\n");  //richiesta stringa
-  fgets(messaggio, 129, stdin);    // memorizzazo solamente i primi 128 caratteri
-
-  printf("Messaggio iniziale: %s", messaggio);  //ristampo il messaggio iniziale
-  int s;
-
-      do{
+      printf("Inserisci una stringa NON piu lunga di 128 caratteri:\n");  //richiesta stringa
+      fgets(messaggio, 129, stdin);    // memorizzazo solamente i primi 128 caratteri
+      printf("Messaggio iniziale: %s\n", messaggio);  //ristampo il messaggio iniziale
+      while(getchar()!='\n');   // pulisco il buffer
+    do{
           menu();  // invoco il metodo menu dove si potra scegleire
           xor(); // parte il metodo xor che lavora sulle variabili ormai riempite
           printf("\033[0;31m1- Menu\n!1- Esci\n"); //output con colore...
@@ -30,6 +29,7 @@ return 0;
 }
 
 void menu(){
+
   printf("\033[0;31m"); // colore rosso per output
     int s;
     printf("**********************************************\n");
@@ -37,17 +37,21 @@ void menu(){
     printf("* 2- Chiave randomica                        *\n");
     printf("**********************************************\n");
     scanf("%d", &s);
-    if(s==1){
+    switch(s){
+      case 1:
         memset(key, 0, sizeof(key)); // svuoto la key
         manualkey();  // invoco il metodo manuale
-    }else {
+        break;
+      case 2:
         memset(key, 0, sizeof(key)); // svuoto la key
         randomkey();  // (altrimenti) invoco il metodo randomico
+        break;
     }
 }
 
 void randomkey(){
 
+  void inserimento();
   int l= strlen(messaggio);
   time_t t;
   srand((unsigned) time(&t));
@@ -59,29 +63,29 @@ void randomkey(){
 }
 
 void xor(){
-int l = strlen(messaggio);
-for (int i=0; i<l; i++){              // ciclo lungo quanto il Messaggio
-  m_output[i]=key[i]^messaggio[i];    // faccio la combinazione messaggio key
-}
-  printf("Messaggio cifrato: %s\n", m_output);
 
+int l = strlen(messaggio);
+
+for (int i=0; i<l; i++){              // ciclo lungo quanto il Messaggio
+  m_output[i]=key[i]^messaggio[i];   // faccio la combinazione messaggio key
+}
+printf("Messaggio cifrato: %s\n", m_output);
   char m;
   for (int i=0; i<l; i++){       // ciclo lungo quanto il Messaggio
     m=m_output[i]^key[i];        // metto su una variabile d'appoggio la combinazione cifrato e chiave
     m_output[i]=m;              // metto il risultato sulla sua cella...
   }
-    printf("Messaggio decifrato: %s\n", m_output);
-
+  printf("Messaggio decifrato: %s\n", m_output);
 }
 
 void manualkey(){
 
-  while(getchar()!='\n');   // pulisco il buffer
   do{
+      while(getchar()!='\n');   // pulisco il buffer
       printf("\033[0;34mInserisci una chiave di Max 128 caratteri e NON piu corta del Messaggio:\n");
       fgets(key, 129, stdin);
   }while(strlen(key)<strlen(messaggio));  // ripeto l'input fino a che la key sara maggiore o uguale al messaggio
   printf("Chiave manuale: %s\n", key);
-
+  while(getchar()!='\n'); 
 }
 // author @andim
